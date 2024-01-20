143fd5e4020393eb2d9314e41a06704959046c60
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-20 19:36:34 +0300

    fix 02-write-file task according to the application: add path-module and fix readline module for correct work

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,32 @@
 // const readline = require('readLine');
 const fs = require('fs');
-const readline = require('readline'); 
+const readline = require('readline');
+const path = require('path');
 
+const PATH = path.join('02-write-file', 'greeting.txt');
 
 let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'What is your name?      '
+    prompt: 'What is your name? '
 });
 
 
-fs.open('./02-write-file/greeting.txt', 'w', (err) => {
+fs.open(PATH, 'w', (err) => {
   if(err) throw err;
   rl.prompt();
-  rl.on('line', (name) => {
-    fs.appendFile('./02-write-file/greeting.txt', `Hi, ${name}!`, (err) => {
-      if(err) throw err;
-      console.log(`   Hi, ${name}!`);
-      rl.close();
+  rl.on('SIGINT', () => {
+    console.log('Text is added to the file')
+    rl.close();
   });
+  rl.on('line', (answer) => {
+    if (answer !== 'exit') {
+      fs.appendFile('./02-write-file/greeting.txt', `\n${answer}`, (err) => {
+        if(err) throw err;
+      })
+    } else {
+      console.log('Text is added to the file')
+      rl.close();
+    }
   });
 });
