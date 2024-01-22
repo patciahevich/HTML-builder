8da0f2137a0dc8f063057a45c7d3947c2da2a95c
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-22 11:04:26 +0300

    fix: solve 04-copy-directory according to application

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 const SRC = path.join('04-copy-directory', 'files');
-const DIR = path.join('/04-copy-directory', 'files-copy')
+const DEST = path.join('04-copy-directory', 'files-copy');
 
-fs.
 
+fs.mkdir(DEST, { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.readdir(DEST, { withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.unlink(path.join(DEST, file.name), () => {})})
+  });
+  fs.readdir(SRC, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.copyFile(path.join(SRC, file.name), path.join(DEST, file.name), () => {})
+    });
+  });
+});
