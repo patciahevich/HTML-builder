5f91d0dab4b031dbb272bb00039e7eaf15dd082e
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-22 11:31:37 +0300

    feat: solve 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const BUNDLE = path.join('05-merge-styles', 'project-dist', 'bundle.css');
+
+fs.open(BUNDLE, 'w', (err) => {
+  if(err) throw err;
+
+  fs.readdir(
+    path.join('05-merge-styles', 'styles'),
+    { withFileTypes: true },
+    (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      if (path.extname(path.join(file.path, file.name)) === '.css') {
+        fs.readFile(path.join(file.path, file.name), 'utf-8', (err, data) => {
+          if (err) throw err;
+          fs.appendFile(BUNDLE, data, () => {})
+        })
+      }
+    })
+  })
+});
+
