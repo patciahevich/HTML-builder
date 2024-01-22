d9648c12b50b2eb152a4c95aea3ca9911b999b7b
Volha Patciahevich <zaprudskaya@gmail.com>
2024-01-22 22:35:43 +0300

    refactor: change 04-copy-directory using promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,22 +4,21 @@ const path = require('path');
 const SRC = path.join('04-copy-directory', 'files');
 const DEST = path.join('04-copy-directory', 'files-copy');
 
-fs.mkdir(DEST, { recursive: true }, (err) => {
-  if (err) throw err;
-  fs.readdir(DEST, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-      fs.unlink(path.join(DEST, file.name), () => {});
-    });
-  });
-  fs.readdir(SRC, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-      fs.copyFile(
-        path.join(SRC, file.name),
-        path.join(DEST, file.name),
-        () => {},
-      );
-    });
-  });
-});
+(async function copyDirectory(src, dest) {
+  await fs.promises.mkdir(dest, { recursive: true });
+
+  const oldFiles = await fs.promises.readdir(dest);
+  for (let file of oldFiles) {
+    await fs.promises.unlink(path.join(dest, file))
+  }
+
+  const assets = await fs.promises.readdir(src);
+  for (let asset of assets) {
+    let stat = await fs.promises.stat(path.join(src, asset));
+    if (stat.isDirectory()) {
+      await copyDirectory(path.join(src, asset), path.join(dest, asset));
+    } else {
+      await fs.promises.copyFile(path.join(src, asset), path.join(dest, asset));
+    }
+  }
+})(SRC, DEST);
